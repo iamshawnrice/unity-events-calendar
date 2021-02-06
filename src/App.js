76d65fe684e895ceed041c5b118e96bf2311da48
Shawn Rice <iamshawnrice@gmail.com>
2021-02-05 22:58:23 -0500

    wip - wire up google cal api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { useState, useEffect } from 'react';
+import gapis from 'googleapis';
+
+function getEvents() {
+    const calendar = gapis.calendar_v3({
+        auth: 'AIzaSyCTKogCA5Q_LiG4VIM0WvZO7pluIWP3vkY'
+    });
+
+    calendar.events.list({
+        'calendarId': 
+    });
+}
+
 function App() {
-    return (
-        <h1>Hello</h1>
-    );
+    const [ isLoading, setIsLoading ] = useState(true);
+    const [ events, setEvents ] = useState([]);
+
+    return isLoading ? <h1>Loading...</h1> : <pre>{ JSON.stringify(events) }</pre> 
 }
 
 export default App;
