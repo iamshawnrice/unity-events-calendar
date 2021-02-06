4c095362f29adb68dbeef0687431b27279a576a2
Shawn Rice <iamshawnrice@gmail.com>
2021-02-05 23:25:32 -0500

    remove api key from source code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import { useState, useEffect } from 'react';
-import gapis from 'googleapis';
+import { useState } from "react";
+import gapis from "googleapis";
+
+import keys from "./gapis.keys.json";
 
 function getEvents() {
-    const calendar = gapis.calendar_v3({
-        auth: 'AIzaSyCTKogCA5Q_LiG4VIM0WvZO7pluIWP3vkY'
-    });
+  const calendar = gapis.calendar_v3({
+    auth: keys.apiKey,
+  });
 
-    calendar.events.list({
-        'calendarId': 
+  calendar.events
+    .list({
+      calendarId: "l20qk6u5qoa84jvslc69em9v3s@group.calendar.google.com",
+    })
+    .then((response) => {
+      console.log(response);
     });
 }
 
 function App() {
-    const [ isLoading, setIsLoading ] = useState(true);
-    const [ events, setEvents ] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [events, setEvents] = useState([]);
 
-    return isLoading ? <h1>Loading...</h1> : <pre>{ JSON.stringify(events) }</pre> 
+  getEvents();
+
+  return isLoading ? <h1>Loading...</h1> : <pre>{JSON.stringify(events)}</pre>;
 }
 
 export default App;
